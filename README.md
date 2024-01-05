e62f387cba00c4a7a24bf3333cf274762d412311
SHIRIN2223 <asgbtdz@gmail.com>
2024-01-06 02:50:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Redux Toolkit İş Projesi
+
+# Kütüphaneler
+- react-router-dom
+- axios
+- sass
+- react-toastify
+- json-server
+- react-redux
+- @reduxjs/toolkit
+- uuid
+- react-icons
+
+
+
+
+# Redux-Toolkit-job-app
---
+++ src/components/Loading.jsx
@@ -0,0 +1,18 @@
+
+const Loading = () => {
+  return (
+    <div className="loader">
+    <div className="loader-square"></div>
+    <div className="loader-square"></div>
+    <div className="loader-square"></div>
+    <div className="loader-square"></div>
+    <div className="loader-square"></div>
+    <div className="loader-square"></div>
+    <div className="loader-square"></div>
+  </div>
+   
+   
+  )
+}
+
+export default Loading;
